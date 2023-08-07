c146a439c3aabbbf02cf27c474b62010c9e71981
mkorkiak <64260757+mkorkiak@users.noreply.github.com>
2023-08-07 15:36:18 +0300

    Added more functionality
    Added a new parameter z0 (roughness length).
    If z0 is given, Kljun2015 uses equations that use z0 to estimate the footprints. Otherwise, umean & ustar relation is used like before.
    It is now possible to set sector-wise displacement heights (d) and z0 that are used to calculate the footprints.
    Added a new parameter D_Z0_SECTORS_LOC, which is the path to the xlsx or csv file containing the sector-wise d and z0.
    If sector-wise d and z0 are used, the 30 min time series of aerodynamic measurement height (zm), d, z0 and recalculated stability parameter are saved into a separate file into SAVE_LOC.

--- Footprints_Kljun15_KM2001.py
+++ Footprints_Kljun15_KM2001.py
@@ -36,25 +36,38 @@ import pandas as pd
 #Location of the eddypro full output datafile
 DATA_LOC = '/home/m/Desktop/eddypro_full_output_adv.csv'
 
+#Location of the sector-wise d and z0 file.
+#Set to None if no sector-wise values are used.
+#If sector-wise d and z0 are used, they with the aerodynamic measurement height
+#and recalculated stability parameters will be saved into a separate file.
+D_Z0_SECTORS_LOC = '/home/m/Desktop/d_and_z0.xlsx'
+
 #Folder to save the footprints
 SAVE_LOC = '/home/m/Desktop/'
 
 #Measurement height
-MEAS_HEIGHT = 26
+MEAS_HEIGHT = 7
 
 #Canopy height
 #Either canopy height or displacement height is required. Canopy height is used
 #to calculate the displacement height, if the displacement height is not given.
 #If DISP_HEIGHT is given, CANOPY_HEIGHT is ignored.
 #Use the same canopy height as in Eddypro!!!
-CANOPY_HEIGHT = 21
+CANOPY_HEIGHT = 2
 
 #Displacement height
 #Set to None if not known. Don't use zero.
 #If DISP_HEIGHT is None, the displacement height will be calculated from the
 #CANOPY_HEIGHT.
 #Use the same displacement height as in Eddypro!!!
-DISP_HEIGHT = 9
+DISP_HEIGHT = None
+
+#Roughness length
+#Set to None if not known. Don't use zero.
+#If Z0 is None, it is not used to calculate footprints. Instead, alternative
+#methods are used.
+#Use the same roughness length as in Eddypro!!!
+Z0 = None
 
 #Latitude of the measurements, needed for Kljun et al. 2015
 #Needs to be 0 <= lat <= 90
@@ -67,12 +80,12 @@ DO_KLJUN15 = True
 #Calculate footprints according to Kormann & Meixner, 2001
 #Must be True or False
 #KM2001 gives generally larger footprints and is 6-7 times slower than Kljun15.
-DO_KM01 = True
+DO_KM01 = False
 
 
 
 #########################################################################################
-VERSION = 'v1.0 MAR 2023'
+VERSION = 'v1.1 AUG 2023'
 APPNAME = 'Footprints_Kljun15_KM2001'
 
 #Ignore warnings. I know what I'm doing.
@@ -157,22 +170,78 @@ def check_params(MEAS_HEIGHT, DISP_HEIGHT, CANOPY_HEIGHT, LAT, SAVE_LOC,
     if (LAT < 0 or LAT > 90) and DO_KLJUN15 is True:
         sys.exit('Latitude has to be within 0 <= LAT <= 90. Closing the program.')
 
-def calc_DISP_HEIGHT(CANOPY_HEIGHT):
+def load_d_z0_file(D_Z0_SECTORS_LOC):
+    """Loads the file containing sector-wise displacement height (d) and
+    roughness length (z0) parameters.
+        
+    Parameters:
+        D_Z0_SECTORS_LOC (str): Path to the sector-wise d and z0 file. 
+                
+    Returns:
+        d_z0_data (dataframe): Dataframe containing sector-wise d and z0 
+        parameters.
+    """
+    try:
+        d_z0_data = pd.read_excel(D_Z0_SECTORS_LOC)
+    
+    #If the file is not an excel file. Try loading it as a csv.
+    except (FileNotFoundError, ValueError):
+        try:
+            d_z0_data = pd.read_csv(D_Z0_SECTORS_LOC)
+        except: #If any error after this. Close the program
+            if CANOPY_HEIGHT is not None or DISP_HEIGHT is not None:
+                print("Sector-wise displacement height and z0 file was not found. " + 
+                      "Using one displacement height for the whole site.")
+                d_z0_data=np.nan
+            
+            else:
+                sys.exit("Sector-wise displacement height and z0 file was not found. " +
+                         "Also, CANOPY_HEIGHT and DISP_HEIGHT is not set! " +
+                         "Closing the program.")
+    
+    return d_z0_data
+
+def disp_z0_height_mngmnt(DISP_HEIGHT, CANOPY_HEIGHT, d_z0_data):
     """Calculate the displacement height if it is not given. Use the same
     method as Eddypro uses.
     For forest canopies, displacement height is estimated to vary between 0.6
     and 0.8 times the height of the canopy (Arya, 1998; Stull, 1988).
         
     Parameters:
+        DISP_HEIGHT (float): Displacement height.
         CANOPY_HEIGHT (float): Canopy height.
+        
                 
     Returns:
-        DISP_HEIGHT (float): Displacement height.
+        d_z0_data (DataFrane): Dataframe containing sector-wise displacement 
+                               and z0 heights.
     """
-    DISP_HEIGHT = 0.67 * CANOPY_HEIGHT #Same as in Eddypro
-
-    return DISP_HEIGHT
-
+    #If the file containing sector-wise d and z0 data has been loaded successfully
+    if isinstance(d_z0_data, pd.DataFrame):
+        return d_z0_data
+    
+    #If one DISP_HEIGHT value is given, use it for all the wind sectors.
+    elif DISP_HEIGHT is not None:
+        d_z0_data = pd.DataFrame({'Start_WD [deg]':np.arange(0, 360, 10),
+                        'End_WD [deg]':np.arange(10, 370, 10), 'd [m]': DISP_HEIGHT, 
+                        'z0 [m]': Z0}, index=[np.arange(0, 36)])
+        return d_z0_data
+        
+    #If sector-wise d and z0 data does not exist and DISP_HEIGHT is not given
+    elif DISP_HEIGHT is None:
+        try:
+            DISP_HEIGHT = 0.67 * CANOPY_HEIGHT
+        except TypeError: #If canopy height does not exist, close the program
+            raise TypeError('DISP_HEIGHT or CANOPY_HEIGHT is not given! Also, ' +
+                            'no sector-wise d or z0 data was not found! One ' +
+                            'of the three has to be given to run the program!')
+            
+        d_z0_data = pd.DataFrame({'Start_WD [deg]':np.arange(0, 360, 10),
+                        'End_WD [deg]':np.arange(10, 370, 10), 'd [m]': DISP_HEIGHT, 
+                        'z0 [m]': Z0}, index=[np.arange(0, 36)])
+        return d_z0_data
+    
+    
 def epro_data_load(DATA_LOC):
     """Loads and formats the eddypro full output data for easier handling by converting
     the time information to datetime and dropping unnecessary columns. Calculates
@@ -216,7 +285,68 @@ def epro_data_load(DATA_LOC):
 
     return data, data_units
 
-def bounday_layer_height(Ls, ustars, t_covs, LAT, zm, air_ts):
+def calc_zm_stabparam(data, data_cols, d_z0_data, SAVE_LOC):
+    """Calculate height above the measurement height for all the data rows and also 
+    recalculate the stability parameter for all the data rows.
+        
+    Parameters:
+        data (dataframe): Eddypro full output data in pandas dataframe.
+        data_units (series): Units of the data in pandas series.
+        d_z0_data (dataframe): Dataframe containing sector-wise displacement 
+                               and z0 heights.
+        SAVE_LOC (str): The folder where the result file is saved.
+                
+    Returns:
+        data (dataframe): Eddypro full output data in pandas dataframe with updated
+                          height above the measurement height, stability parameter,
+                          and z0 values.
+    """
+    winddir_bins = np.linspace(0, 360, 37) #Bin limits for wind dirs
+    labels = np.arange(0,36) #Labels for the bins (index of d_z0_data)
+    
+    #Bind the data according to wind sectors
+    wd_bins = pd.cut(data["wind_dir"], bins = winddir_bins, labels = labels)
+    wd_bins = wd_bins.astype(float) #Bin names to float
+    
+    #If wind dir in the data was nan, set the label to zero. It does not matter
+    #what the label actually is as flux is also nan.
+    wd_bins[wd_bins.isnull()] = 0
+    
+    #Calculate the height below the measurement height and get d and z0 for all datarows
+    zm = MEAS_HEIGHT - d_z0_data['d [m]'][wd_bins]
+    d = d_z0_data['d [m]'][wd_bins]
+    z0 = d_z0_data['z0 [m]'][wd_bins]
+    
+    #Add zm and z0 columns to the data dataframe
+    zm.index = z0.index = d.index = data.index
+    data['zm'] = zm
+    data['d'] = d
+    data['z0'] = z0
+    
+    #Add zm and z0 units to data_cols
+    data_cols = pd.concat([data_cols, pd.Series(['[m]','[m]','[m]'], 
+                                                index = ['zm', 'd', 'z0'])])
+    
+    #Recalculate the stability parameter for all datarows and add it to data
+    #dataframe, replacing the old values.
+    data['(z-d)/L'] = data.zm / data.L
+    
+    #If using sector-wise values, save the 30-min values of zm, d, z0 and (z-d)/L
+    #into a separate file.
+    if np.std(d_z0_data['d [m]']) != 0:
+        #Get the variables of interest
+        data_params = data[['zm','d','z0','(z-d)/L']]
+        
+        #Reset index
+        data_params = data_params.reset_index()
+    
+        #Change the datetime column (previously the index) name
+        data_params = data_params.rename(columns = {'index':'Datetime (Period end)'})
+        data_params.to_csv(SAVE_LOC + 'new_zm_d_z0.csv', index = False)
+
+    return data
+
+def bounday_layer_height(Ls, ustars, t_covs, LAT, zLs, air_ts):
     """Calculates the boundary layer height according to Kljun et al. 2015,
     Appendix B. During stable stratification, the height is calculated directly.
     During unstable stratification, the height is calculated iteratively by
@@ -227,7 +357,7 @@ def bounday_layer_height(Ls, ustars, t_covs, LAT, zm, air_ts):
         ustars (series): Series containing the friction velocities.
         t_covs (series): Series containing the temperature covariances.
         LAT (float): Latitude.
-        zm (float): The height above the displacement height.
+        zLs (series): Series containing the stability parameters.
         air_ts (series): Series containing air temperatures.
                 
     Returns:
@@ -235,7 +365,6 @@ def bounday_layer_height(Ls, ustars, t_covs, LAT, zm, air_ts):
     """
     omg = 7.2921159e-5 #Angular velocity of Earth's rotation
     f = -2 * omg * np.sin(LAT) #Coriolis parameter
-    zLs = zm / Ls #Stability parameter
 
     #If zeta larger than this, startification is considered neutral or stable
     neutral_limit = 0
@@ -282,16 +411,19 @@ def bounday_layer_height(Ls, ustars, t_covs, LAT, zm, air_ts):
 
     return hs
 
-def get_contribution_dist(r, zm, h, umean, ustar):
+def get_contribution_dist(r, zm, h, umean, ustar, z0, psi, use_z0):
     """Calculate the distance where the relative contribution of interest is
     obtained in Kljun15 for each 30 min period.
         
     Parameters:
         r (float): The relative contribution [0, 1] that is calculated.
         zm (float): The height above the displacement height.
-        h (series): Series containing the planetary boundary layer heights.
-        umean (series): Series containing the mean wind speeds.
-        ustar (series): Series containing the friction velocities.
+        h (float): The planetary boundary layer height.
+        umean (float): The mean wind speed.
+        ustar (float): The friction velocity.
+        z0 (float): The roughness length.
+        psi (float): The non-dimensional wind shear
+        use_z0 (boolean): Which equation is used to calculate the contribution
                 
     Returns:
         xr (series): Series containing the distances of the corresponding relative
@@ -303,13 +435,16 @@ def get_contribution_dist(r, zm, h, umean, ustar):
     #Eq. 17 in Kljun2015
     c = 1.4622
     d = 0.1359
-
-    #Eq. 25 in Kljun2015
-    xr = (-c / np.log(r) + d) * (zm * (1 - zm / h)**-1) * umean / ustar * vk
+    
+    if use_z0 == True:
+        xr = (-c / np.log(r) + d) * (zm * (1 - zm / h)**-1) * (np.log(zm / z0) - psi)
+   
+    else:  #Eq. 25 in Kljun2015
+        xr = (-c / np.log(r) + d) * (zm * (1 - zm / h)**-1) * umean / ustar * vk
 
     return xr
 
-def kljun_2015(zLs, ustars, umeans, hs, zm):
+def kljun_2015(zLs, ustars, umeans, hs, zms, Ls, z0s):
     """Calculate the footprints according to Kljun et al. 2015. The
     footprints are calculated by using the mean wind speed. Roughness length
     is not used.
@@ -319,7 +454,9 @@ def kljun_2015(zLs, ustars, umeans, hs, zm):
         ustars (series): Series containing the friction velocities.
         umeans (series): Series containing the mean wind speeds.
         hs (series): Series containing the planetary boundary layer heights.
-        zm (float): The height above the displacement height.
+        zms (series): Series containing the heights above the displacement height.
+        Ls (series): Series containing the obukhov lengths
+        z0s (series): Series containing the roughness lengths
         
     Returns:
         fps (dataframe): Dataframe containing the footprints calculated with
@@ -333,7 +470,7 @@ def kljun_2015(zLs, ustars, umeans, hs, zm):
     #the height of the entrainment layer (Eq. 27 in Kljun2015), set those
     #boundary layer heights to nan (=makes also resulting footprints nan) as
     #those cases break the assumptions of Kljun2015.
-    hs[(hs < 10) | (zm > 0.8 * hs)] = np.nan
+    hs[(hs < 10) | (zms > 0.8 * hs)] = np.nan
 
     #If ustar < 0.1 m s-1 or the stability parameter is < -15.5,
     #Kljun2015 is not valid, set those cases nan.
@@ -353,24 +490,42 @@ def kljun_2015(zLs, ustars, umeans, hs, zm):
     p60 = np.int(total * 0.6)
     p80 = np.int(total * 0.8)
     line_nums = pd.Series(np.arange(len(zLs)), index=zLs.index)
-    
+
     fps = pd.DataFrame(dtype=float)
-    for ind, ustar, umean, h in zip(ustars.index, ustars, umeans, hs):
+    for ind, ustar, umean, h, L, zm, z0 in zip(ustars.index, ustars, umeans, 
+                                               hs, Ls, zms, z0s):
         #Print progress
         print_progress(np.array([p20, p40, p60, p80]), line_nums[ind], 'Kljun2015')
         
         #Maximum footprint contribution
-        #Eq. 22 in Kljun2015
-        xr_peak = 0.87 * zm / (1. - (zm / h)) * (umean / ustar * vk)
+        #If z0 exists, use the format of the equation where it is used 
+        #(Eq. 22 in Kljun2015)
+        #Otherwise use Eq. 21 in Kljun2015 (based on ratio of umean and ustar)
+        if z0 is not None and np.isnan(z0) == False and z0 > 0: #Eq 22
+            #Calculate the non-dimensional wind shear, psi
+            if L > 0: #Stable stratification
+                psi = -5.3 * zm / L
+            else: #Unstable stratification
+                khi = (1 - 19 * zm / L)**0.25
+                psi = (np.log((1 + khi**2) / 2) + 2 * np.log((1 + khi) / 2) - 2 * 
+                np.arctan(khi) + np.pi/2)
+            
+            #Calculate the peak footprint distance
+            xr_peak = 0.87 * zm / (1. - (zm / h)) * (np.log(zm / z0) - psi)
+            use_z0 = True
+        else: #Eq. 21
+            xr_peak = 0.87 * zm / (1. - (zm / h)) * (umean / ustar * vk)
+            psi = np.nan
+            use_z0 = False
 
         #Footprint offset
-        xr_offset = get_contribution_dist(0.01, zm, h, umean, ustar)
+        xr_offset = get_contribution_dist(0.01, zm, h, umean, ustar, z0, psi, use_z0)
 
         #Calculate the distances where the relative contributions are obtained
-        xr_50 = get_contribution_dist(0.5, zm, h, umean, ustar)
-        xr_60 = get_contribution_dist(0.6, zm, h, umean, ustar)
-        xr_70 = get_contribution_dist(0.7, zm, h, umean, ustar)
-        xr_80 = get_contribution_dist(0.8, zm, h, umean, ustar)
+        xr_50 = get_contribution_dist(0.5, zm, h, umean, ustar, z0, psi, use_z0)
+        xr_60 = get_contribution_dist(0.6, zm, h, umean, ustar, z0, psi, use_z0)
+        xr_70 = get_contribution_dist(0.7, zm, h, umean, ustar, z0, psi, use_z0)
+        xr_80 = get_contribution_dist(0.8, zm, h, umean, ustar, z0, psi, use_z0)
 
         temp = pd.DataFrame({'x_offset':xr_offset, 'x_peak':xr_peak,
                            'x_50%':xr_50, 'x_60%':xr_60, 'x_70%':xr_70,
@@ -396,7 +551,7 @@ def print_progress(lims, ind, method):
             print ("Calculating " + method + " footprints. 60% done...")
         if ind == lims[3]:
             print ("Calculating " + method + " footprints. 80% done...")
-            
+
 def korm_meix(zLs, ustars, umeans, zm):
     """Calculate the footprints according to Kormann & Meixner (2001). The
     footprints are calculated by using the mean wind speed. Roughness length
@@ -427,19 +582,19 @@ def korm_meix(zLs, ustars, umeans, zm):
     p60 = np.int(total * 0.6)
     p80 = np.int(total * 0.8)
     line_nums = pd.Series(np.arange(len(zLs)), index=zLs.index)
-
+    
     for ind, zL, ustar, umean in zip(zLs.index, zLs, ustars, umeans):
         #Print progress
-        print_progress_km(np.array([p20, p40, p60, p80]), line_nums, 'K&M2001')
+        print_progress(np.array([p20, p40, p60, p80]), line_nums[ind], 'K&M2001')
         
         #Similarity relations (Paulson, 1970)
         #Eqs. 33, 34 and 35 in KM2001
         if zL > 0:
             phi_m = 1 + 5 * zL
+            phi_c = 1 + 5 * zL
         else:
             phi_m = (1 - 16 * zL)**(-1/4)
-            
-        phi_c = (1 - 16 * zL)**(-1/2)
+            phi_c = (1 - 16 * zL)**(-1/2)
 
         #Intermediate parameters for K&M2001
         #Exponent of the diffusivity power law
@@ -562,18 +717,22 @@ def main(DISP_HEIGHT):
     #Check that the given parameters make sense
     check_params(MEAS_HEIGHT, DISP_HEIGHT, CANOPY_HEIGHT, LAT, SAVE_LOC,
                      DO_KLJUN15, DO_KM01)
-
-    #Calculate the displacement height if it is not given
-    if DISP_HEIGHT is None:
-        DISP_HEIGHT = calc_DISP_HEIGHT(CANOPY_HEIGHT)
+    
+    #Load sector-wise displacement height and z0 datafile
+    d_z0_data = load_d_z0_file(D_Z0_SECTORS_LOC)
+    
+    #Prepare displacement height and roughness length data for the program, 
+    #depending on the input.
+    d_z0_data = disp_z0_height_mngmnt(DISP_HEIGHT, CANOPY_HEIGHT, d_z0_data)
 
     #Load and format the Eddypro data
-    data, _ = epro_data_load(DATA_LOC)
+    data, data_cols = epro_data_load(DATA_LOC)
 
     print("\nEddypro datafile loaded and formatted.\n")
-
-    #Measurement height above the displacement height
-    zm = MEAS_HEIGHT - DISP_HEIGHT
+    
+    #Calculate zm and recalculate stability parameters for all wind sectors
+    #and save the results into a separate file.
+    data = calc_zm_stabparam(data, data_cols, d_z0_data, SAVE_LOC)
 
     if DO_KLJUN15 is True:
         #Start timer
@@ -581,12 +740,13 @@ def main(DISP_HEIGHT):
 
         #Calculate boundary layer heights for Kljun et al. 2015
         print("Calculating planetary boundary layer heights.")
-        hs = bounday_layer_height(data.L, data['u*'], data['w/ts_cov'], LAT, zm,
-                               data.air_temperature)
+        hs = bounday_layer_height(data.L, data['u*'], data['w/ts_cov'], LAT, 
+                                  data['(z-d)/L'], data.air_temperature)
 
         #Calculate cross-wind integrated footprint according to Kljun et al. 2015
         print("Calculating Kljun et al. (2015) footprints.")
-        fps_kljun = kljun_2015(data['(z-d)/L'], data['u*'], data.wind_speed, hs, zm)
+        fps_kljun = kljun_2015(data['(z-d)/L'], data['u*'], data.wind_speed, 
+                               hs, data.zm, data.L, data.z0)
 
         #Save the footprints
         save_files(fps_kljun, SAVE_LOC, 'Kljun2015')
@@ -607,7 +767,7 @@ def main(DISP_HEIGHT):
 
         #Calculate cross-wind integrated footprint according to Kormann & Meixner 2001
         print("Calculating Kormann & Meixner (2001) footprints.")
-        fps_km = korm_meix(data['(z-d)/L'], data['u*'], data.wind_speed, zm)
+        fps_km = korm_meix(data['(z-d)/L'], data['u*'], data.wind_speed, data.zm)
 
         #Save the footprints
         save_files(fps_km, SAVE_LOC, 'K&M2001')
